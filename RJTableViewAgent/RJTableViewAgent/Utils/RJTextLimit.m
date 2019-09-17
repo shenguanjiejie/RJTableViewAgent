2cbd5177a290f526c6b03eb89fff26f13e40a660
shenruijie_vendor <shenruijie_vendor@sensetime.com>
2019-09-17 20:08:07 +0800

    🕰2019-09-17 20:03:32🕰   再次调整正则验证逻辑

--- RJTableViewAgent/RJTableViewAgent/Utils/RJTextLimit.m
+++ RJTableViewAgent/RJTableViewAgent/Utils/RJTextLimit.m
@@ -16,43 +16,60 @@
 
 
 + (BOOL)validateWithText:(NSString *)text limit:(RJTextLimitType)limit{
-    if (limit == RJTextLimitNone) {
+    if ((limit & RJTextLimitNone) == RJTextLimitNone) {
         return YES;
     }
     
-    if ((limit & RJTextLimitNumber) == RJTextLimitNumber && [self matchesRegex:RJTextLimitNumberRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
-        return YES;
+    if ((limit & RJTextLimitNumber) == 0 && (limit & RJTextLimitNumberAll) == 0 && [self matchesRegex:RJTextLimitNumberRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+        return NO;
     }
     
-    if ((limit & RJTextLimitNumberAll) == RJTextLimitNumberAll && [self matchesRegex:RJTextLimitNumberAllRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
-        return YES;
+    /**RJ 2019-09-17 22:16:32
+     如果不包含RJTextLimitNumberAll, 而且验证通过了,而且包含RJTextLimitNumber,验证又未通过,那么就说明是类似0\d类型的数字, 所以return NO
+     */
+    if ((limit & RJTextLimitNumberAll) == 0 && [self matchesRegex:RJTextLimitNumberAllRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+        if ((limit & RJTextLimitNumber) == RJTextLimitNumber) {
+            if (![self matchesRegex:RJTextLimitNumberRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+                return NO;
+            }
+        }else{
+            return NO;
+        }
     }
     
-    if ((limit & RJTextLimitLetter) == RJTextLimitLetter && [self matchesRegex:RJTextLimitLetterRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
-        return YES;
+    if ((limit & RJTextLimitLetter) == 0 && [self matchesRegex:RJTextLimitLetterRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+        return NO;
     }
     
-    if ((limit & RJTextLimitChinese) == RJTextLimitChinese && [self matchesRegex:RJTextLimitChineseRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
-        return YES;
+    if ((limit & RJTextLimitChinese) == 0 && [self matchesRegex:RJTextLimitChineseRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+        return NO;
     }
     
-    if ((limit & RJTextLimitSymbol) == RJTextLimitSymbol && [self matchesRegex:RJTextLimitSymbolRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
-        return YES;
+    if ((limit & RJTextLimitSymbol) == 0 && [self matchesRegex:RJTextLimitSymbolRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+        return NO;
     }
     
-    if ((limit & RJTextLimitEmoji) == RJTextLimitEmoji && [self matchesRegex:RJTextLimitEmojiRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
-        return YES;
+    if ((limit & RJTextLimitEmoji) == 0 && [self matchesRegex:RJTextLimitEmojiRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+        return NO;
     }
     
-    if ((limit & RJTextLimitDecimal) == RJTextLimitDecimal && [self matchesRegex:RJTextLimitDecimalRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
-        return YES;
+    /**RJ 2019-09-17 22:10:35 如果既不包含RJTextLimitDecimal也不包含RJTextLimitDecimal2, 却又是一个小数,return NO*/
+    if ((limit & RJTextLimitDecimal) == 0 && (limit & RJTextLimitDecimal2) == 0 && [self matchesRegex:RJTextLimitDecimalRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+        return NO;
     }
     
-    if ((limit & RJTextLimitDecimal2) == RJTextLimitDecimal2 && [self matchesRegex:RJTextLimitDecimal2Regex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
-        return YES;
+    /**RJ 2019-09-17 22:14:24 与上方RJTextLimitNumber和RJTextLimitNumberAll逻辑相同*/
+    if ((limit & RJTextLimitDecimal2) == 0 && [self matchesRegex:RJTextLimitDecimal2Regex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+        if ((limit & RJTextLimitDecimal) == RJTextLimitDecimal) {
+            if (![self matchesRegex:RJTextLimitDecimalRegex withString:text options:NSRegularExpressionDotMatchesLineSeparators]) {
+                return NO;
+            }
+        }else{
+            return NO;
+        }
     }
     
-    return NO;
+    return YES;
 }
 
 + (BOOL)matchesRegex:(NSString *)regex withString:(NSString *)string options:(NSRegularExpressionOptions)options {
